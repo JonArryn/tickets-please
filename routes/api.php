6f791d3f9c65f059fd63092187a4b10d9719e543
Jon Arryn <development@arryn.net>
2024-11-07 23:50:44 -0500

    Implements token authentication with login and logout

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +8,7 @@ Route::prefix('v1')->group(base_path('routes/api_v1.php'));
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
