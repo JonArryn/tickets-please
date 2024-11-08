af61c36c305bf8d6d869a1ff94cc6df2637833d8
Jon Arryn <development@arryn.net>
2024-11-07 22:36:10 -0500

    Implement API Versioning and creates Ticket controller and requests

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TicketController;
 use App\Http\Controllers\AuthController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(base_path('routes/api_v1.php'));
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::apiResource('tickets', TicketController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+
