907affa22beb2bd2d43fb231ef3656a1df5bc573
Jon Arryn <development@arryn.net>
2024-11-09 23:04:22 -0500

    Implements replacing ticket data with PUT requests

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -6,9 +6,14 @@ use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->apiResource('ticket', TicketController::class);
-Route::middleware('auth:sanctum')->apiResource('authors', AuthorController::class);
-Route::middleware('auth:sanctum')->apiResource('authors.tickets', AuthorTicketsController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('ticket', TicketController::class)->except(['update']);
+    Route::put('ticket/{ticket}', [TicketController::class, 'replace']);
+
+    Route::apiResource('authors', AuthorController::class);
+    Route::apiResource('authors.tickets', AuthorTicketsController::class)->except(['update']);
+    Route::put('authors/{author}/tickets/{ticket}', [AuthorTicketsController::class, 'replace']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
